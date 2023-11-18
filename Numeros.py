7dc6a59a2b2049766582b81393080628de7beaf4
Carlos Hernández <124458348+CarlosAHernandezC@users.noreply.github.com>
2023-11-18 12:52:17 -0300

    Numeros.py

--- Numeros.py
+++ Numeros.py
@@ -1,16 +1,14 @@
-# Archivo: generar_numeros.py
 import os
 
-# Directorio de trabajo donde se ubicará el archivo
-directorio_trabajo = 'C:/Users/ch_us/OneDrive - Universidad San Sebastian/Diplomado Cloud/Material/Clase_24'  # Reemplaza con la ruta deseada
+# Directorio de trabajo donde se ubicará el archivo (directorio actual)
+directorio_trabajo = os.getcwd()
 
 # Asegúrate de que el directorio exista
 os.makedirs(directorio_trabajo, exist_ok=True)
 
-# Ruta completa al archivo
+# Ruta completa al archivo en el directorio de trabajo
 ruta_archivo = os.path.join(directorio_trabajo, 'numeros2.txt')
 
 with open(ruta_archivo, 'w') as file:
     for i in range(1, 100001):
         file.write(str(i) + '\n')
-
